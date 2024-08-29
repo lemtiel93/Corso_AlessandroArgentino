59d7fe697d3985ab4716e4231e0217f48ea61239
Alessandro Argentino <160162269+lemtiel93@users.noreply.github.com>
2024-08-29 12:52:36 +0200

    Add files via upload

---
+++ corso-data-science/28-08/operatori_lineari.py
@@ -0,0 +1,11 @@
+x = 5 
+y = 10
+z = 7
+
+print(x < y and y > z) # True
+print(x < y or z > y) # True
+print (not(x < y)) # False
+print(x == y) # False
+print(x != y) # True
+print ( x < y) # True
+
---
+++ corso-data-science/28-08/prova_stringhe.py
@@ -0,0 +1,5 @@
+s = "Ciao, mondo!"
+print(len(s))
+print(s.upper())
+print(s.split('o'))
+print(s.replace('Ciao','salve'))
