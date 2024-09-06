80440fe174ec79d7224b1a4c9ba9b5133ac572ca
Alessandro Argentino <alessandro.argentino93@gmail.com>
2024-09-06 16:13:24 +0200

    Update comprimi_stringa.py
    aggiunta commenti a comprimi stringa

--- corso-data-science/06-09/comprimi_stringa.py
+++ corso-data-science/06-09/comprimi_stringa.py
@@ -1,3 +1,4 @@
+# Decoratore che controlla se stringa inserita è uguale in lunghezza a stringa compressa
 def decoratore(funzione):
     def wrapper(stringa):
         stringa_compressa = funzione(stringa)
@@ -7,19 +8,26 @@ def decoratore(funzione):
             return stringa_compressa
     return wrapper 
 
+# Funzione per comprimere stringa
 @decoratore
 def comprimi_stringa(stringa):
     new_stringa = ''
     count = 1
+    
+    # Itero stringa partendo da 1 e confronto con valore precedente
     for i in range(1, len(stringa)):
         if stringa[i - 1] == stringa[i]:
             count +=1
         else:
             new_stringa += stringa[i - 1] + str(count)
             count = 1
+    
+    # Aggiungo ultimo carattere che il for non stampa
     new_stringa += stringa[-1] + str(count)
     return new_stringa
 
+
+# Funzione per chiedere in input stringa e controllarne se il valore è di soli caratteri alfabetici
 def inserisci_stringa():
     while True:
         stringa = input("Inserisci stringa di soli caratteri ")
@@ -27,6 +35,7 @@ def inserisci_stringa():
             return stringa
         else:
             continue
-        
+
+# Test della funzione       
 print(comprimi_stringa(inserisci_stringa()))
 
