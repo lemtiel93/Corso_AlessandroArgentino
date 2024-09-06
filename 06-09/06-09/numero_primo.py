1b11174ee9b3bd7317de5d30d393c287c2264124
Alessandro Argentino <160162269+lemtiel93@users.noreply.github.com>
2024-09-06 14:47:19 +0200

    Add files via upload

---
+++ 06-09/06-09/numero_primo.py
@@ -0,0 +1,29 @@
+def decoratore(funzione):
+    def wrapper():
+        nome , numero = funzione()
+        for i in range(2,numero):
+            if numero % i == 0:
+                return numero // i
+            return True
+    return wrapper
+        
+        
+
+    
+@decoratore
+def inserisci_nome_numero():
+    nome = input("Inserisci nome: ")
+    numero = int(input("Inserisci numero: "))
+    return nome,numero
+    
+print(inserisci_nome_numero())
+    
+
+
+
+        
+
+    
+    
+    
+        
