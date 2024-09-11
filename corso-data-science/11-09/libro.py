5b948f33ec0badd7906c8a56dbb7b9f5eb8b78a0
Alessandro Argentino <alessandro.argentino93@gmail.com>
2024-09-11 11:36:29 +0200

    inserimento libro e libro2

--- corso-data-science/11-09/libro.py
+++ corso-data-science/11-09/libro.py
@@ -2,19 +2,19 @@ class Libro:
     
     def __init__(self, titolo, autore, pagine):
         
-            if type(titolo == str):
-                self.titolo = titolo
-            else: 
-                print("Il titolo deve essere una stringa")
-            if type(autore == str):
-                self.autore = autore
-            else:
-                print("Autore deve essere una stringa")
-            if type (pagine == int):
-                self.pagine = pagine
-            else:
-                print("Pagine deve essere numero")
-            
+        if type(titolo == str):
+            self.titolo = titolo
+        else: 
+            print("Il titolo deve essere una stringa")
+        if type(autore == str):
+            self.autore = autore
+        else:
+            print("Autore deve essere una stringa")
+        if type (pagine == int):
+            self.pagine = pagine
+        else:
+            print("Pagine deve essere numero")
+        
     def descrizione(self):
         print(f"Titolo: {self.titolo} Autore: {self.autore} Pagine: {self.pagine}")
         
