1fd27dc5615f5250991633fa098ebfe3683c0c97
Alessandro Argentino <alessandro.argentino93@gmail.com>
2024-09-11 11:26:47 +0200

    Update libro.py

--- corso-data-science/11-09/libro.py
+++ corso-data-science/11-09/libro.py
@@ -1,17 +1,26 @@
 class Libro:
     
     def __init__(self, titolo, autore, pagine):
-        try:
-            if type(titolo== str) and type (autore == str) and type(pagine == int):
+        
+            if type(titolo == str):
                 self.titolo = titolo
+            else: 
+                print("Il titolo deve essere una stringa")
+            if type(autore == str):
                 self.autore = autore
+            else:
+                print("Autore deve essere una stringa")
+            if type (pagine == int):
                 self.pagine = pagine
-        except: 
-            print("errore")
+            else:
+                print("Pagine deve essere numero")
             
     def descrizione(self):
-        print(f"Titolo :{self.titolo} Autore: {self.autore} Pagine: {self.pagine}")
+        print(f"Titolo: {self.titolo} Autore: {self.autore} Pagine: {self.pagine}")
         
-libro = Libro("Il visconte dimezzato", "Italo Calvino", 5)
+libro = Libro("Il visconte dimezzato", 54, "5")
 
-libro.descrizione()
+if type(libro == Libro):
+    libro.descrizione()
+else: 
+    print("Errore di tipo")
