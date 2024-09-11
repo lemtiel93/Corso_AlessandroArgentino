dbdd08b5d8b5ccc8c81d5d1b3a6ef495709193fa
Alessandro Argentino <alessandro.argentino93@gmail.com>
2024-09-11 17:36:33 +0200

    commit prodotto

---
+++ corso-data-science/11-09/prodotto.py
@@ -0,0 +1,24 @@
+class Prodotto:
+    def __init__(self, descrizione, costo_produzione, prezzo_vendita):
+        
+        if type(descrizione) == str and type(costo_produzione) == int and type(prezzo_vendita) == int:
+            self.descrizione = descrizione
+            self.costo_produzione = costo_produzione
+            self.prezzo_vendita = prezzo_vendita
+        else:
+            raise ValueError("Errore inserimento dati")
+        
+    def calcola_profitto(self):
+        return self.prezzo_vendita - self.costo_produzione
+
+class Xbox(Prodotto):
+    
+    def __init__(self , descrizione , costo_produzione, prezzo_vendita , serie):
+        super.__init__(descrizione , costo_produzione , prezzo_vendita)
+        self.serie = serie
+
+class Computer(Prodotto):
+    
+    def __init__(self , descrizione , costo_produzione, prezzo_vendita , processore):
+        super.__init__(descrizione , costo_produzione , prezzo_vendita)
+        self.serie = processore
